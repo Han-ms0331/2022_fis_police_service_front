df4d6a0cf0187456025e40a27cc0023cca66d2ff
한명수 <hanmyeongsu@hanmyeongsus-iMac.local>
2022-01-10 10:35:28 +0900

    add store directory

--- src/components/templates/Navigation.js
+++ src/components/templates/Navigation.js
@@ -5,8 +5,11 @@ function Navigation(props) {
     return (
         <div>
             <Link to={"/main"}>시설관리</Link>
+            <br/>
             <Link to={"/schedule"}>일정관리</Link>
+            <br/>
             <Link to={"/manage"}>관리자</Link>
+            <br/>
             <Link to={"/"}>로그아웃</Link>
         </div>
     );
