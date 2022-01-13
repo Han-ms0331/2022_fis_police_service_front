dda702ae701cbc5019dd6eb96d793faa6a04feb0
신은수 <shin-eunsu@sin-eunsuui-MacBookAir.local>
2022-01-13 10:50:41 +0900

    creating list and listContainer

--- src/components/pages/MainPage.js
+++ src/components/pages/MainPage.js
@@ -2,33 +2,38 @@ import React, {useEffect, useState} from 'react';
 import Navigation from "../templates/Navigation";
 import Grid from "@material-ui/core/Grid";
 import {Redirect} from "react-router-dom";
+import List from "../molecules/List";
+import ListContainer from "../organisms/ListContainer";
 
 
 function MainPage(props) {
     return (
 
-        props.isLogined?    //props로 받은 islogined state에 따라 보여주는 창이 달라짐
-            (
-        <div>
-            <Grid container spacing={1}>
-                <Grid item xs={1}>
-                    <Navigation/>
-                </Grid>
+        // props.isLogined?    //props로 받은 islogined state에 따라 보여주는 창이 달라짐
+        //     (
+        // <div>
+        //     <Grid container spacing={1}>
+        //         <Grid item xs={1}>
+        //             <Navigation/>
+        //         </Grid>
+        //
+        //         <Grid item xs={8}>
+        //             <div style={ {height: "100vh", borderRight: "1px solid #6D5A00"}}>
+        //                 main body template
+        //             </div>
+        //         </Grid>
+        //
+        //         <Grid item xs={3}>
+        //             main info template
+        //         </Grid>
+        //     </Grid>
+        // </div>
+        //     )
+        //     :
+        //     <Redirect to={"/"} />
 
-                <Grid item xs={8}>
-                    <div style={ {height: "100vh", borderRight: "1px solid #6D5A00"}}>
-                        main body template
-                    </div>
-                </Grid>
+        <ListContainer/>
 
-                <Grid item xs={3}>
-                    main info template
-                </Grid>
-            </Grid>
-        </div>
-            )
-            :
-            <Redirect to={"/"} />
     );
 }
 
