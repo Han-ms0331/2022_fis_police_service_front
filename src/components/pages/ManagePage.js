bee5d24f85f366350eb8824e774703ac32a176b1
신은수 <shin-eunsu@sin-eunsuui-MacBookAir.local>
2022-01-13 16:43:49 +0900

    developing

--- src/components/pages/ManagePage.js
+++ src/components/pages/ManagePage.js
@@ -2,30 +2,24 @@ import React from 'react';
 import styled from 'styled-components';
 import Navigation from "../templates/Navigation";
 import {Redirect} from "react-router-dom";
-import CenterManageTemp from "../templates/CenterManageTemp";
-import centerManageTemp from "../templates/CenterManageTemp";
-import ScheduleModifyInputForm from "../organisms/ScheduleModifyInputForm";
-import CenterManageInputForm from "../organisms/CenterManageInputForm";
-import AgentManageInputForm from "../organisms/AgentManageInputForm";
-import UserManageInputForm from "../organisms/UserManageInputForm";
 
 function ManagePage(props) {
-    // let isLogined;      //로그인 상태에 따라 랜더링을 결정하는 변수
-    // if (localStorage.getItem("loginStatus") === "true") //localstorage에서 loginStatus가 true일때 isLogined를 true로 바꿔줌
-    //     isLogined= true;
-    // else
-    //     isLogined = false;
-    // return (
-    //     isLogined ?     //isLogined가 false면 redirect를 시킴
-    //         (
-    //     <Main>
-    //                 <Navigation/>
-    //                 manage
-    //     </Main>
-    // )   :
-    //         <Redirect to={"/"} />
-    // )
-    return <CenterManageTemp />
+    let isLogined;      //로그인 상태에 따라 랜더링을 결정하는 변수
+    if (localStorage.getItem("loginStatus") === "true") //localstorage에서 loginStatus가 true일때 isLogined를 true로 바꿔줌
+        isLogined= true;
+    else
+        isLogined = false;
+    return (
+        isLogined ?     //isLogined가 false면 redirect를 시킴
+            (
+        <Main>
+                    <Navigation/>
+                    manage
+        </Main>
+    )   :
+            <Redirect to={"/"} />
+    )
+
 }
 const Main = styled.div`
     display: grid;
