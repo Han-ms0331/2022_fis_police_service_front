f05de0b92347574223c6f1cb60bc8266cee215e6
cy <cy@cyui-MacBookAir.local>
2022-01-13 14:03:15 +0900

    ScheduleTable2

--- src/store/ScheduleSearchKeyword.js
+++ src/store/ScheduleSearchKeyword.js
@@ -3,7 +3,7 @@
     이름: 이창윤
     작성내용: 검색 키워드 정보들을 담아 놓는 Recoil Atom
     ScheduleTableSearch에서 atom value를 변경하고,
-    ScheduleTable에서 atom value를 불러 오는 데 사용될 파일
+    ScheduleTable에서 atom value를 불러 오는 데 사용될 파일..
 */
 
 import {atom} from "recoil";
