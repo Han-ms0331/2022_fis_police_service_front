c816e1aac6be165ddbcfdf52e834a8b6de9a2a00
신은수 <shin-eunsu@sin-eunsuui-MacBookAir.local>
2022-01-13 17:26:29 +0900

    about modal

--- src/components/pages/ManagePage.js
+++ src/components/pages/ManagePage.js
@@ -3,26 +3,28 @@ import styled from 'styled-components';
 import Navigation from "../templates/Navigation";
 import {Redirect} from "react-router-dom";
 
+
 function ManagePage(props) {
     let isLogined;      //로그인 상태에 따라 랜더링을 결정하는 변수
     if (localStorage.getItem("loginStatus") === "true") //localstorage에서 loginStatus가 true일때 isLogined를 true로 바꿔줌
-        isLogined= true;
+        isLogined = true;
     else
         isLogined = false;
     return (
         isLogined ?     //isLogined가 false면 redirect를 시킴
             (
-        <Main>
+                <Main>
                     <Navigation/>
                     manage
-        </Main>
-    )   :
-            <Redirect to={"/"} />
+                </Main>
+            ) :
+            <Redirect to={"/"}/>
     )
 
 }
+
 const Main = styled.div`
-    display: grid;
+  display: grid;
   grid-template-columns: 3% 97%;
   height: 100vh;
 `;
