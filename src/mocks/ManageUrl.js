c325cb9833a4b36433edc383ce0ba331dd819a96
신은수 <shin-eunsu@sin-eunsuui-MacBookAir.local>
2022-01-18 10:44:34 +0900

    받아온 데이터 화면에 띄우기

--- src/mocks/ManageUrl.js
+++ src/mocks/ManageUrl.js
@@ -7,31 +7,6 @@ import {rest} from "msw";
 */
 
 export const manageHandlers = [
-    rest.get('/main/center/search?c_name={value}&c_address={value}&c_ph={value}', async (req, res, ctx) => {
-        return res(
-            ctx.json({
-                lists: [
-                    {
-                        center_id: "1",
-                        center_name: "동그라미 유치원",
-                        participation: "참여",
-                        c_ph: "031-123-456",
-                        c_address: "경기도 안양시 동안구 동안로 111"
-                    },
-                    {
-                        center_id: "2",
-                        center_name: "딩동댕 유치원",
-                        participation: "보류",
-                        c_ph: "031-123-456",
-                        c_address: "경기도 안양시 동안구 동안로 111"
-                    }
-                ],
-                result: "success"
-                }
-
-            )
-        )
-    }),
     rest.post('/manage/center', async (req, res, ctx) => {
         return res(
             ctx.json({
