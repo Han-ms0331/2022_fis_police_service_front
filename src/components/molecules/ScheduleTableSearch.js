385bb2f91ae304865f8b888b1aca51742092c0b5
cy <cy@cyui-MacBookAir.local>
2022-01-18 14:46:49 +0900

    schedule patch/post & ModifyInputForm DefaultValue

--- src/components/molecules/ScheduleTableSearch.js
+++ src/components/molecules/ScheduleTableSearch.js
@@ -29,12 +29,12 @@ function ScheduleTableSearch() {
             <TableHead>
                 <TableRow>
                     <TableCell />
-                    <TableCell align="left"><TextField size="small" id="No" label="No." variant="outlined" onChange={handleChange}/></TableCell>
-                    <TableCell align="right"><TextField size="small" id="agent" label="현장요원" variant="outlined" onChange={handleChange}/></TableCell>
-                    <TableCell align="right"><TextField size="small" id="center" label="시설정보" variant="outlined" onChange={handleChange}/></TableCell>
-                    <TableCell align="right"><TextField size="small" id="etc" label="특이사항" variant="outlined" onChange={handleChange}/></TableCell>
-                    <TableCell align="right"><TextField size="small" id="change" label="변경 사항" variant="outlined" onChange={handleChange}/></TableCell>
-                    <TableCell align="right"><TextField size="small" id="call" label="통화 이력" variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="left"><TextField size="small" id="index" label="No." variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="right"><TextField size="small" id="a_name" label="현장요원" variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="right"><TextField size="small" id="c_name" label="시설정보" variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="right"><TextField size="small" id="total_etc" label="특이사항" variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="right"><TextField size="small" id="modified_info" label="변경 사항" variant="outlined" onChange={handleChange}/></TableCell>
+                    <TableCell align="right"><TextField size="small" id="call_check" label="통화 이력" variant="outlined" onChange={handleChange}/></TableCell>
                     <TableCell align="right"><TextField size="small" id="notice" label="일정 공지" variant="outlined" onChange={handleChange}/></TableCell>
                     <TableCell />
                 </TableRow>
