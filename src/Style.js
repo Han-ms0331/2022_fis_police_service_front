281fbcc6f84dcf78c2674ac09e840b833274c585
hongdaniel <jungd1223@naver.com>
2022-01-19 16:23:50 +0900

    scheduleBody margin

--- src/Style.js
+++ src/Style.js
@@ -1,3 +1,4 @@
+const font = {}
 
 const colors = {
     option1: {
@@ -8,21 +9,22 @@ const colors = {
     option2: {
         color1: "#FCF6F5", //흰색
         color2: "#7b9acc", //하늘색
+        color3: "rgba(46, 60, 127, 0.16)"
     },
     option3: {
         color1: "#FEFEFE",   //흰색
-        color2: "#8AAAE5"     //하늘색...
+        color2: "#8AAAE5",     //하늘색...
+        color3: "rgba(46, 60, 127, 0.16)"
     },
     option4: {
         color1: "#50586C",   //쥐색..?
-        color2: "#DCE2F0"   //회보라색..?
+        color2: "#DCE2F0",   //회보라색..?
+        color3: "rgba(46, 60, 127, 0.16)"
     }
 };
-const font = {
 
-}
 export const Style = {
-    color1:colors.option1.color1,
-    color2:colors.option1.color2,
+    color1: colors.option1.color1,
+    color2: colors.option1.color2,
     color3: colors.option1.color3
 };
