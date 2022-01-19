096742f754f4a45608c01c0cedfe773b0da3cae1
지상은 <chisang-eun@jisang-eun-ui-MacBookAir.local>
2022-01-19 16:41:00 +0900

    map marker color change

--- src/components/molecules/RangeController.js
+++ src/components/molecules/RangeController.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CustomLabel from "../atoms/CustomLabel";
 import CustomButton from "../atoms/CustomButton";
+import {Style} from "../../Style";
 
 /*
     날짜 : 2022/01/12 2:14 PM
@@ -15,11 +16,11 @@ function RangeController(props) {
                 <div style={{display: 'flex', alignItems: 'center'}}>
                     <div style={{marginRight:"5px"}}>주변시설</div>
                     <CustomButton type={"normal"} width={"10px"} height={"20px"} color={"white"} borderRadius={"2"}
-                                  backgroundColor={"orange"} name={"250"} content={"250m"} onClick={props.onClickFunc}/>
+                                  backgroundColor={Style.color2} name={"250"} content={"250m"} onClick={props.onClickFunc}/>
                     <CustomButton type={"normal"} width={"10px"} height={"20px"} color={"white"} borderRadius={"2"}
-                                  backgroundColor={"orange"} name={"500"} content={"500m"} onClick={props.onClickFunc}/>
+                                  backgroundColor={Style.color2} name={"500"} content={"500m"} onClick={props.onClickFunc}/>
                     <CustomButton type={"normal"} width={"10px"} height={"20px"} color={"white"} borderRadius={"2"}
-                                  backgroundColor={"orange"} name={"1000"} content={"1km"} onClick={props.onClickFunc}/>
+                                  backgroundColor={Style.color2} name={"1000"} content={"1km"} onClick={props.onClickFunc}/>
                 </div>
 
         </>
