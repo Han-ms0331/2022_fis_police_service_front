d6ee8418a73c6139347b4dd8a67ad0eaa33db00f
hongdaniel <jungd1223@naver.com>
2022-01-25 16:07:04 +0900

    채팅기능 구현

--- src/components/organisms/MessangerList.js
+++ src/components/organisms/MessangerList.js
@@ -7,6 +7,7 @@ import {Style} from "../../Style";
 import {useRecoilValue} from "recoil";
 import {Messages} from "../../store/Message";
 import {Message} from "../molecules/Message";
+import NetworkConfig from "../../configures/NetworkConfig";
 /*
 날짜: 2022/01/11 11:27 AM
 작성자: 정도식
@@ -19,24 +20,32 @@ import {Message} from "../molecules/Message";
 message msw 작업 완료
 */
 const MessangerList = () => {
-    const [messages,setMessages]=useState([]);
-    const getData = async () => {
-        axios.get("/messenger").then((res) => {
-            setMessages(res.data);
-        });
-    }
+    const [messages, setMessages] = useState([]);
 
-    useEffect(()=>{ //최초 렌더링시 서버로부터 데이터를 받아온다.
-getData()
-    },[]);
+    const getData = () => {
+        const ws = new WebSocket(`ws://${NetworkConfig.networkAddress}/messenger/websocket`);
+        ws.onmessage = (data) => {
+            setMessages(data.data);
+        }
+    };
+
+    useEffect(() => { //최초 렌더링시 서버로부터 데이터를 받아온다.
+        getData();
+        console.log(messages);
+    }, []);
 
     const handleDone = () => { // 수정완료버튼을 눌렀을 경우...
         console.log("수정완료!")
     }
     return (
         <Main>
-            {messages.map((msg,idx)=>{
-                return <Message key={idx} header={msg.time} agent={msg.user} content={msg.message} handleDone={handleDone}/>}
+            <Message header={"test"} agent={"test user"} content={"메시지 내용"}
+                     handleDone={handleDone}/>
+            {messages.map((msg, idx) => {
+                console.log(msg);
+                // return <Message key={idx} header={msg.time} agent={msg.user} content={msg.message}
+                    //                 handleDone={handleDone}/>
+                }
             )}
         </Main>
     );
