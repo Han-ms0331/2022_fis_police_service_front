18c1c29eb533825dee7052a8cb7c16c1b0ac9a16
지상은 <chisang-eun@jisang-eun-ui-MacBookAir.local>
2022-01-26 14:58:34 +0900

    많은 것을 바꿈

--- src/components/atoms/CustomPolyLine.js
+++ src/components/atoms/CustomPolyLine.js
@@ -22,7 +22,7 @@ function CustomPolyLine(props) {
             ]}
             */
             strokeWeight={5} // 선의 두께 입니다
-            strokeColor={Style.color2} // 선의 색깔입니다
+            strokeColor="#17BA5C" // 선의 색깔입니다
             strokeOpacity={0.7} // 선의 불투명도 입니다 1에서 0 사이의 값이며 0에 가까울수록 투명합니다
             /*strokeStyle={"solid"} // 선의 스타일입니다*/
         />
