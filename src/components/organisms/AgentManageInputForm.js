41961810d4d948b42246b72e9b154f30854fed6e
신은수 <shin-eunsu@sin-eunsuui-MacBookAir.local>
2022-03-16 16:23:14 +0900

    사진 완성

--- src/components/organisms/AgentManageInputForm.js
+++ src/components/organisms/AgentManageInputForm.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, {createRef, useEffect, useRef, useState} from 'react';
 import InputContainer from "../molecules/InputContainer";
 import CustomButton from "../atoms/CustomButton";
 import {Style} from "../../Style";
-import {styled} from '@mui/material/styles';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import PhotoCamera from '@mui/icons-material/PhotoCamera';
-import Stack from '@mui/material/Stack';
 import CustomLabel from "../atoms/CustomLabel";
+import question from '../media/question.png';
+import {Checkbox} from "@mui/material";
+import CheckboxContainer from "../molecules/CheckboxContainer";
 
-const Input = styled('input')({
-    display: 'none',
-});
 
 function AgentManageInputForm(props) {
+    const [selectedImage, setSelectedImage] = useState(null);
+
+    // This function will be triggered when the file field change
+    const imageChange = (e) => {
+        console.log("hi")
+        if (e.target.files && e.target.files.length > 0) {
+            setSelectedImage(e.target.files[0]);
+        }
+    };
+
+    const handleImgError = (e) => {
+        e.target.src = question;
+    }
+
+
+
     return (
         <div style={{
             padding: "30px 0px",
@@ -82,21 +93,24 @@ function AgentManageInputForm(props) {
                                     setValueFunction={props.handleInputFormChange}
                                     defaultValue={props.currentInfo['a_status']}/>
                 </div>
-                <div style={{marginBottom: "20px", display: "flex", alignItems: "center"}}>
+                <div style={{display: "flex", alignItems: "center"}}>
                     <CustomLabel content={"사진 업로드: "} fontSize={props.fontSize} width={"auto"}/>
-                    <input type="file" id="chooseFile" name="file" accept="image/*"
-                           onChange={props.handleInputFormChange} style={{width: "300px"}}/>
-
+                    <input type="file" id="chooseFile" name="a_picture" accept="image/*"
+                           onChange={(e)=>{
+                               imageChange(e);
+                               props.handleInputFormChange(e)
+                           }} style={{width: "200px"}}/>
                 </div>
-
-                <div style={{marginLeft: "125px"}}>
-                    <img src={`http://${process.env.REACT_APP_IP_ADDRESS}:8080/agent/show?agent_id=${props.agent_id}`}
-                         style={{width: "100px", height: "100px"}}/>
+                <div style={{marginLeft: 125}}>
+                    <CheckboxContainer name="deletePicture" clickFunction={props.clickCheckboxFunction} content="사진삭제" />
+                    <div style={{ marginLeft: 10}}>
+                        <img src={selectedImage===null?`http://${process.env.REACT_APP_IP_ADDRESS}:8080/agent/show?agent_id=${props.currentInfo.agent_id}&time=${new Date().getTime()}`:URL.createObjectURL(selectedImage)}
+                             onError={handleImgError} style={{width: "100px", height: "100px"}}/>
+                    </div>
                 </div>
-
             </div>
 
-            <div style={{display: "flex", marginTop: "20px", position:"absolute", bottom: 0, right: 0}}>
+            <div style={{display: "flex", marginTop: "20px", position: "absolute", bottom: 0, right: 0}}>
                 <div style={{marginRight: "30px"}}>
                     <CustomButton type="reverse" width="150px" height="40px" content="취소" color={Style.color2}
                                   border={`1px solid ${Style.color2}`}
